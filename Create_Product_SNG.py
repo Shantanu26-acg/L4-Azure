09f858e2896a7c421ae5edba4964f73f9eb010d3
Shantanu26-acg <shantanu.mule@acg-world.com>
2026-03-28 13:33:52 +0530

    Update Create_Product_SNG.py

--- Create_Product_SNG.py
+++ Create_Product_SNG.py
@@ -10,7 +10,7 @@ import tempfile
 
 options = Options()
 options.add_argument(f'--user-data-dir={tempfile.mkdtemp()}')
-# options.add_argument("--headless=new")
+options.add_argument("--headless=new")
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
