a246459c591aafc61d2040fd6b1ce222616e1ec1
Shantanu26-acg <shantanu.mule@acg-world.com>
2026-03-28 13:34:51 +0530

    Update Create_role_new.py

--- Create_role_new.py
+++ Create_role_new.py
@@ -88,7 +88,7 @@ import tempfile
 
 options = Options()
 options.add_argument(f'--user-data-dir={tempfile.mkdtemp()}')
-# options.add_argument("--headless")
+options.add_argument("--headless")
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
