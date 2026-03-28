96301d7b0a4441bb56decc3dce58799821c71f4c
Shantanu26-acg <shantanu.mule@acg-world.com>
2026-03-28 16:44:49 +0530

    Update Create_role_new.py

--- Create_role_new.py
+++ Create_role_new.py
@@ -129,16 +129,9 @@ def create_role(driver, wait, role_name, role_desc):
     time.sleep(10)
 
 driver.get(url)
-time.sleep(10)
-# wait.until(EC.presence_of_element_located((By.NAME,'identifier'))).send_keys(username)
-elem=wait.until(EC.element_to_be_clickable((By.NAME,'identifier')))
-driver.execute_script("arguments[0].scrollIntoView();", elem)
-wait.until(EC.element_to_be_clickable((By.NAME, 'identifier')))
-elem.clear()
-elem.send_keys(username)
+wait.until(EC.element_to_be_clickable((By.NAME,'identifier'))).send_keys(username)
 driver.find_element(By.NAME,'password').send_keys(password)
 driver.find_element(By.XPATH, "//button[text()='Login']").click()
-# create_role(driver, wait, "Admin_new", "Testing")
 
 for idx, row in df.iterrows():
     create_role(driver,
