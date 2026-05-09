2344e1d3a74e15f1b096e011c130197b3b0d7ec9
Shantanu26-acg <shantanu.mule@acg-world.com>
2026-05-09 15:23:49 +0530

    Update Create_User_new.py

--- Create_User_new.py
+++ Create_User_new.py
@@ -31,7 +31,7 @@ password=df_logindata.iloc[0, 2]
 print(url, username, password)
 
 def create_user(driver, wait, name, role, email, location, country, phone_number):
-    time.sleep(3)
+    time.sleep(10)
     wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@aria-label='Menu']"))).click()
     wait.until(EC.element_to_be_clickable((By.XPATH, "//span[text()= 'User Management']"))).click()
     driver.find_element(By.XPATH, "//li[text()= 'Add User']").click()
